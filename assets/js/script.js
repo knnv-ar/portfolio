22844f389f4904db91c77523a195781889258c76
knnv-ar <knnv.ar@gmail.com>
2023-01-07 01:10:12 -0300

    Update: theme toggle functionality finished. Fixed: minor bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,17 +4,20 @@
 
 const elemToggleFunc = function (elem) { elem.classList.toggle("active"); }
 
-//header sticky
+//header sticky & go top
 
 const header = document.querySelector("[data-header]");
+const goTopBtn = document.querySelector("[data-go-top]");
 
-window.addEventListener("scroll"), function () {
+window.addEventListener("scroll", function () {
   if (window.scrollY >= 10) {
     header.classList.add("active");
+    goTopBtn.classList.add("active");
   } else {
     header.classList.remove("active");
+    goTopBtn.classList.remove("active");
   }
-};
+});
 
 //navbar toggle
 
@@ -37,8 +40,38 @@ for (let i = 0; i < toggleBtns.length; i++) {
   toggleBtns[i].addEventListener("click", function () {
     elemToggleFunc(toggleBtnBox);
 
-    for (i = 0; i < toggleBtnBox.length; i++) { elemToggleFunc[i]; }
+    for (let i = 0; i < toggleBtnBox.length; i++) { elemToggleFunc[i]; }
 
     elemToggleFunc(skillsBox);
   });
+}
+
+//dark & light theme toggle
+
+const themeToggleBtn = document.querySelector("[data-theme-btn]");
+
+themeToggleBtn.addEventListener("click", function () {
+  elemToggleFunc(themeToggleBtn);
+
+  if (themeToggleBtn.classList.contains("active")) {
+    document.body.classList.remove("dark_theme");
+    document.body.classList.add("light_theme");
+    localStorage.setItem("theme", "light_theme");
+  } else {
+    document.body.classList.add("dark_theme");
+    document.body.classList.remove("light_theme");
+    localStorage.setItem("theme", "dark_theme");
+  }
+});
+
+//check & aplly last time selected theme from localStorage
+
+if (localStorage.getItem("theme") === "light_theme") {
+  themeToggleBtn.classList.add("active");
+  document.body.classList.remove("dark_theme");
+  document.body.classList.add("light_theme");
+} else {
+  themeToggleBtn.classList.remove("active");
+  document.body.classList.remove("light_theme");
+  document.body.classList.add("dark_theme");
 }
